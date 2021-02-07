46a0372cba2093e02c2a58b81a5a1ca18aff5558
Arne Brasseur <arne@arnebrasseur.net>
2021-02-07 10:17:28 +0100

    s/javax.media.opengl/com.jogamp.opengl

--- jbox2d-testbed-jogl/float2double.sh
+++ jbox2d-testbed-jogl/float2double.sh
@@ -14,8 +14,4 @@ sed s/glColor3f/glColor3d/g -i **/*.java
 sed s/glColor4f/glColor4d/g -i **/*.java
 sed 's/glPointSize(/glPointSize((float)/g' -i **/*.java
 sed 's/setColor(color.x, color.y, color.z/setColor((float)color.x, (float)color.y, (float)color.z/' -i **/*.java
-
-# sed 's/int result /long result /g' -i src/main/java/org/jbox2d/testbed/pooling/ColorPool.java
-# sed 's/return result/return (int)result/g' -i src/main/java/org/jbox2d/testbed/pooling/ColorPool.java
-# sed 's/new Color(r, g, b, alpha)/new Color((float)r, (float)g, (float)b, (float)alpha)/' -i src/main/java/org/jbox2d/testbed/framework/j2d/DebugDrawJ2D.java
-# sed 's/g.drawString(s, x, y)/g.drawString(s, (float)x, (float)y)/'  -i src/main/java/org/jbox2d/testbed/framework/j2d/DebugDrawJ2D.java
+sed 's/javax.media.opengl/com.jogamp.opengl/' -i src/**/*.java
